cacf27ac2f983dd7d716311d80d73a86ff46b953
narlo23 <aks152312@gmail.com>
2023-04-03 22:50:35 +0900

    23.04.02

---
+++ UnionFind/1976.py
@@ -0,0 +1,35 @@
+n = int(input())
+m = int(input())
+parents = [i for i in range(n)] #처음 부모는 자기자신
+
+def union(x, y):
+    x = find(x)
+    y = find(y)
+    if x < y:
+        parents[y] = x
+    else:
+        parents[x] = y
+
+def find(x):
+    if x != parents[x]:
+        parents[x] = find(parents[x])
+    return parents[x]
+
+for i in range(n):
+    edge = list(map(int, input().split()))
+    for j in range(n):
+        if edge[j] == 1:
+            union(i, j)
+
+test = list(map(int, input().split()))
+
+result = True
+for i in range(1, m):
+    if parents[test[i]-1] != parents[test[0] - 1]:
+        result = False
+        break
+
+if result:
+    print('YES')
+else:
+    print('NO')
